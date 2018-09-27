b8b0ee4794a14129a0c10ec0cf32783be3f9059b
shehnaaz2208 <39779245+shehnaaz2208@users.noreply.github.com>
2018-09-26 21:00:53 -0400

    Add files via upload

---
+++ errors.php
@@ -0,0 +1,3 @@
+<?php if (count($errors) > 0): ?>
+     <div class="error">
+	 <?php foreach ($errors
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+       <title>User registration system using PHP & MYSQL</title>
+	   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <div class="header">
+	    <h2>Login</h2>
+	</div>
+	
+	<form method="post" action="login.php">
+	    <div class="input-group">
+		     <label>Username</label>
+			 <input type="text" name="username">
+			 </div>
+			 
+			 <div class="input-group">
+		     <label>Password</label>
+			 <input type="text" name="password_1">
+			 </div>
+			  
+			  <div class="input-group">
+		           <button type="submit" name="login" class="btn">Login</button>
+			 </div>
+			 <p>
+			  Not yet a member?  <a href="register.php">Sign up</a>
+			  </p>
+			 
+			 </form>
+			 
+			 </body>
+			 </html>
+	   
